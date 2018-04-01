b5d6bc569cb08ecb3ad1f10d59c73a68ce8eb084
Alexander Parunov <alexander.parunov@gmail.com>
2018-04-01 16:04:37 +0200

    Parsed all xmls and serialized into pickle

--- entities/parser.py
+++ entities/parser.py
@@ -1,30 +1,30 @@
 #!/usr/bin/python3
 from xml_classes import *
 import xml.etree.ElementTree as ET
-from os.path import abspath, join, isdir
+from os.path import abspath, join, isdir, exists
 from os import listdir, makedirs
 import sys
 import pickle
 
-# XML files
+# Each dictionary contains name of dictionary and data, which is paths of all files in specified directory
 train_path = abspath("../data/train/DrugBank")
-drug_bank_train = [join(train_path, f) for f in listdir(train_path)]
+drug_bank_train = {'name': 'drug_bank_train', 'data': [join(train_path, f) for f in listdir(train_path)]}
 
 train_path = abspath("../data/train/MedLine")
-medline_train =   [join(train_path, f) for f in listdir(train_path)]
+medline_train =   {'name':'medline_train', 'data': [join(train_path, f) for f in listdir(train_path)]}
 
 # Test for DDI extraction task
 
 test_path = abspath("../data/test/Test_DDI_Extraction_task/DrugBank")
-drug_bank_ddi_test = [join(test_path, f) for f in listdir(test_path)]
+drug_bank_ddi_test = {'name': 'drug_bank_ddi_test', 'data': [join(test_path, f) for f in listdir(test_path)]}
 test_path = abspath("../data/test/Test_DDI_Extraction_task/MedLine")
-medline_ddi_test =   [join(test_path, f) for f in listdir(test_path)]
+medline_ddi_test =   {'name': 'medline_ddi_test', 'data': [join(test_path, f) for f in listdir(test_path)]}
 
 # Test for DrugNER task
 test_path = abspath("../data/test/Test_DrugNER_task/DrugBank")
-drug_bank_ner_test = [join(test_path, f) for f in listdir(test_path)]
+drug_bank_ner_test = {'name': 'drug_bank_ner_test', 'data': [join(test_path, f) for f in listdir(test_path)]}
 test_path = abspath("../data/test/Test_DrugNER_task/MedLine")
-medline_ner_test =   [join(test_path, f) for f in listdir(test_path)]
+medline_ner_test =   {'name': 'medline_ner_test', 'data': [join(test_path, f) for f in listdir(test_path)]}
 
 class Parser:
     def set_path(self, xml_path):
@@ -58,9 +58,9 @@ class Parser:
                 document.add_sentence(sentence)
         return document
 
-    def parse_save_drug_bank_train(self):
+    def parse_save_xml_dict(self, xml_dict):
         parsed_docs = []
-        for doc in drug_bank_train:
+        for doc in xml_dict['data']:
             print("Parsing: "+doc)
             self.set_path(doc)
             d = self.parse_xml()
@@ -70,14 +70,28 @@ class Parser:
         if not isdir(dir_path):
             makedirs(dir_path)
 
-        with open(join(dir_path, "drug_bank_train.pkl"),"wb") as f:
+        pickle_name = xml_dict['name']+".pkl"
+        with open(join(dir_path, pickle_name),"wb") as f:
             pickle.dump(parsed_docs, f)
-            print("Saved DrugBank parsed documents into pickle.")
+            print("Saved parsed documents from " + pickle_name + " into pickle!\n")
+
+def parse_all_files():
+    parser = Parser()
+    if not exists("../data/pickle/"+drug_bank_train['name']+".pkl"):
+        parser.parse_save_xml_dict(drug_bank_train)
+    if not exists("../data/pickle/"+medline_train['name']+".pkl"):
+        parser.parse_save_xml_dict(medline_train)
+    if not exists("../data/pickle/"+drug_bank_ddi_test['name']+".pkl"):
+        parser.parse_save_xml_dict(drug_bank_ddi_test)
+    if not exists("../data/pickle/"+medline_ddi_test['name']+".pkl"):
+        parser.parse_save_xml_dict(medline_ddi_test)
+    if not exists("../data/pickle/"+drug_bank_ner_test['name']+".pkl"):
+        parser.parse_save_xml_dict(drug_bank_ner_test)
+    if not exists("../data/pickle/"+medline_ner_test['name']+".pkl"):
+        parser.parse_save_xml_dict(medline_ner_test)
 
 def main():
-    parser = Parser()
-    parser.parse_save_drug_bank_train()
-
-
+    parse_all_files()
+    
 if __name__ == "__main__":
     main()
